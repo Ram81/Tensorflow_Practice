0884486ee0a42cd9c45c8f56418a32bb16484776
Ram81 <ramramrakhya81@gmail.com>
2017-06-16 19:13:40 +0530

    Fully Connected Layer added

--- MNIST_Practice/mnistConv.py
+++ MNIST_Practice/mnistConv.py
@@ -45,9 +45,13 @@ b_conv2 = bias_variable([64])
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 h_pool2 = max_pool_2x2(h_conv2)
 
+#Densely Connected Network - a fully connected layer of 1024 neurons
 
+W_fc1 = weight_variable([7 * 7 * 64, 1024])
+b_fc1 = bias_variable([1024])
 
-
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
 
 
