e5090c64209c97984e2df70d513a9534c2462769
Ram81 <ramramrakhya81@gmail.com>
2017-06-16 18:55:57 +0530

    Genralized Conv and Pooling

--- MNIST_Practice/mnistConv.py
+++ MNIST_Practice/mnistConv.py
@@ -13,3 +13,10 @@ def bias_variable(shape):
 	initial = tf.constant(0.1, shape = shape)
 	return tf.Variable(initial)
 
+def conv2d(x, W):
+	return tf.nn.conv2d(x, W, strides = [1, 1, 1, 1], padding = 'SAME')
+
+def max_pool_2x2(x):
+	return tf.nn.max_pool(x, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = 'SAME')
+
+
