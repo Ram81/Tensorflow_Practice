648e7e3832f71d3ce5d89abb663cc9a45e9d53ff
Ram81 <ramramrakhya81@gmail.com>
2017-06-16 19:11:12 +0530

    Convolutional and Pooling Layers added

--- MNIST_Practice/mnistConv.py
+++ MNIST_Practice/mnistConv.py
@@ -1,9 +1,18 @@
 import os
 import numpy as np
 import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
 
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 tf.logging.set_verbosity(tf.logging.ERROR)
+
+mnist = input_data.read_data_sets('MNIST_data/', one_hot = True)
+sess = tf.InteractiveSession()
+
+#input and output tensors
+x = tf.placeholder(tf.float32, [None, 784])
+y_ = tf.placeholder(tf.float32, [None, 10])
+
 
 def weight_variable(shape):
 	initial = tf.truncated_normal(shape, stddev = 0.1)
@@ -18,5 +27,28 @@ def conv2d(x, W):
 
 def max_pool_2x2(x):
 	return tf.nn.max_pool(x, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = 'SAME')
+
+#First Convolutional Layer - 32 features and 5x5 patch
+W_conv1 = weight_variable([5, 5, 1, 32])
+b_conv1 = bias_variable([32])
+
+x_image = tf.reshape(x, [-1, 28, 28, 1])
+
+h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+h_pool1 = max_pool_2x2(h_conv1)
+
+
+#Second Convolutional Layer - 64 features and 5x5 patch
+W_conv2 = weight_variable([5, 5, 1, 64])
+b_conv2 = bias_variable([64])
+
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+h_pool2 = max_pool_2x2(h_conv2)
+
+
+
+
+
+
 
 
