12aca390715918a58ab590b669754ca487705b6d
Ram81 <ramramrakhya81@gmail.com>
2017-06-16 19:26:06 +0530

    Training and Evaluating Model

--- MNIST_Practice/mnistConv.py
+++ MNIST_Practice/mnistConv.py
@@ -65,5 +65,22 @@ b_fc2 = bias_variable([10])
 
 y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
+#Traing and Evaluate network
 
+cross_entropy =  tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels = y_, logits = y_conv))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+
+correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+sess.run(tf.global_variables_initializer())
+
+for i in range(20000):
+	batch = mnist.train.next_batch(50)
+	if i%100 == 0:
+		train_accuracy = accuracy.eval(feed_dict = {x: batch[0], y_: batch[1], keep_prob : 1.0})
+		print(" Step %d, Training Accuracy %g"%(i, train_accuracy))
+		train_step.run(feed_dict = {x: batch[0], y_: batch[1], keep_prob : 0.5})
+
+print("Test Accuracy %d"%accuracy.eval(feed_dict = {x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
 
