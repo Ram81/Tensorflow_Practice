bb9dc2b12be1d37bcf705f6061941ce4a013ed34
Ram81 <ramramrakhya81@gmail.com>
2018-01-19 08:39:17 +0530

    CNN Complete

--- MNIST_Practice/mnist_deep.py
+++ MNIST_Practice/mnist_deep.py
@@ -7,6 +7,12 @@ from tensorflow.examples.tutorials.mnist import input_data
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 tf.logging.set_verbosity(tf.logging.ERROR)
 
+# Reading input
+mnist = input_data.read_data_sets('MNIST_data/', one_hot=True)
+
+X = tf.placeholder(tf.float32, shape=[None, 784])
+Y = tf.placeholder(tf.float32, shape=[None, 10])
+
 def weight_variable(shape):
 	'''
 		Generates a Weight Variable of given shape 
@@ -66,3 +72,21 @@ W_fc2 = weight_variable([1024, 10])
 B_fc2 = bias_variable([10])
 
 Y_conv = tf.matmul(H_fc1_dropout, W_fc2) + B_fc2
+
+# Train & Evaluate Model
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=Y_conv, labels=Y))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+
+correct_prediction = tf.equal(tf.argmax(Y, 1), tf.argmax(Y_conv, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+with tf.Session() as session:
+	session.run(tf.global_variables_initializer())
+	for i in range(200):
+		batch = mnist.train.next_batch(50)
+		if i%100 == 0:
+			train_accuracy = accuracy.eval(feed_dict={X: batch[0], Y: batch[1], keep_prob: 1.0})
+			print('Training accuracy in step %d is %g'%(i, train_accuracy))
+		train_step.run(feed_dict={X: batch[0], Y: batch[1], keep_prob: 0.5})
+
+	print('Test accuracy is %g'%(accuracy.eval(feed_dict={X: mnist.test.images, Y: mnist.test.labels, keep_prob: 1.0})))
